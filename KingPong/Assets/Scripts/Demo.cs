5254703f74e5f711e81eca7fcbe4246483f26246
CaptNemo <jironomics2@gmail.com>
2018-05-02 20:47:17 -0700

    added some folders n stuff

---
+++ KingPong/Assets/Scripts/Demo.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Demo : MonoBehaviour {
+
+	//hey I just made this so you have a shell to work off of
+}
