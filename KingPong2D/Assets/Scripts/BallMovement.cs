bd7f0325d78d1c3f969ad18df4b21477702e363c
Unknown <gibari@gmail.com>
2018-05-10 11:11:26 -0700

    Added my version of  pong 2d, Lemme know if it compiles

---
+++ KingPong2D/Assets/Scripts/BallMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour {
+
+    public float speed = 5f;
+    Vector3 dirV3;
+    Vector2 dir;
+    Rigidbody rb;
+  
+	// Use this for initialization
+	void Start () {
+        rb = gameObject.GetComponent<Rigidbody>();
+        Spawn();
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.x > 6 || transform.position.x < -6)
+        {
+            Spawn();
+        }
+	}
+
+    void Spawn() {
+        transform.position = Vector3.zero;
+        dir = Random.insideUnitCircle;
+        dirV3 = new Vector3(dir.x, dir.y);
+        rb.velocity = dirV3 * speed;
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Paddle")
+        {
+            Vector3 paddle = col.transform.position;
+            Vector3 angle = paddle - transform.position;
+            float mag = Vector3.Magnitude(angle);
+            rb.velocity = -angle * speed;
+        }
+    }
+}
---
+++ KingPong2D/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+   
+    public float speed = 10f;
+    public bool isPlayerOne =true;
+    public float yMax = 4;
+    public float yMin = -4;
+
+    void Update() {
+        float input = 0f;
+        
+        if (isPlayerOne)
+        {
+            input = Input.GetAxisRaw("Vertical");
+        }
+        else {
+            input = Input.GetAxisRaw("Vertical2");
+        }
+
+        Vector3 Pos = gameObject.transform.position;
+        Pos.y += speed * Time.deltaTime * input;
+       Pos.y= Mathf.Clamp(Pos.y, yMin, yMax);
+        gameObject.transform.position = Pos;
+      
+       
+
+
+
+
+    }
+}
