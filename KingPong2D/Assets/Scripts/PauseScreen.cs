3eb51c2cee5c8ac31c82a43b0698b3447bfef4f0
Unknown <gibari@gmail.com>
2018-05-21 05:56:09 -0700

    Added start screen and leaderboard, also some WIP test scenes for other features

---
+++ KingPong2D/Assets/Scripts/PauseScreen.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PauseScreen : MonoBehaviour {
+
+
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenu;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+	}
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+
+    }
+
+    public void Pause() {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+    public void Leaderboard()
+    {
+        SceneManager.LoadScene("LeaderBoard");
+    }
+    public void StartMenu()
+    {
+        SceneManager.LoadScene("Start");
+    }
+
+
+}
---
+++ KingPong2D/Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class TitleScreen : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Begin() {
+
+        SceneManager.LoadScene("Game");
+    }
+    public void Leaderboard() {
+
+        SceneManager.LoadScene("LeaderBoard");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
