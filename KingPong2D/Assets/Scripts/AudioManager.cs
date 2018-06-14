cd6e85c92173cf41d7d5c1ad8785034f092807cb
Unknown <gibari@gmail.com>
2018-06-13 18:17:01 -0700

    Refined the main game loop, added hooks for ball hit and round events

---
+++ KingPong2D/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class AudioManager : MonoBehaviour {
+
+    public delegate Event play(int i);
+    public AudioClip[] files;
+    public AudioSource player;
+
+	// Use this for initialization
+	void Start () {
+        player = Camera.main.GetComponent<AudioSource>();
+        BallMovement.ballHit += PlayOnce();
+            }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlayOnce(int i) {
+        if (i < files.Length) {
+            player.PlayOneShot(files[i]);
+
+        }
+
+    }
+}
